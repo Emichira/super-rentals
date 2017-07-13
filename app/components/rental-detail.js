7f1d169a57d8ca606cbc87f5debabbd3436be937
EMichira <emmanuelmichira@gmail.com>
2017-07-13 13:00:19 +0300

    added functionality of deleting both sides of a model relationship

--- app/components/rental-detail.js
+++ app/components/rental-detail.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
       if (confirm('Are you sure you want to delete this rental?')) {
         this.sendAction('destroyRental', rental);
       }
+    },
+    destroyReveiew(review) {
+      this.sendAction('destroyReveiew', review);
     }
   }
 });
